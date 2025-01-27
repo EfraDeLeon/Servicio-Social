e758955bf5f384080b1a84e3f4ba453fb071df15
Efra De León <alconnix@gmail.com>
2025-01-27 15:09:43 -0600

    UpdateEne1.9

---
+++ Programas del servicio/1° Bimestre/Enero/Java/workspace/Proyecto24_01_2025/src/PDL/Suma.java
@@ -0,0 +1,29 @@
+package PDL;
+
+import javax.*;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		String primerNumero;
+		String segundoNumero;
+		
+		int numero1;
+		int numero2;
+		int suma;
+		
+		//primerNumero = JOptionPane.showInputDailog("Escriba el primer entero" );
+		//segundoNumero = JOptionPane.showInputDailog("Escriba el segundo entero" );
+		
+		//numero1 = Integer.parseInt(primerNumero);
+		//numero2 = Integer.parseInt(segundoNumero);
+		
+		//suma = numero1 + numero2;
+		
+		//JOptionPane.ShowMessageDialog(null, "La suma es " + suma, "Resultados", JOptionPane.PLAIN_MESSAGE);
+		
+		System.exit(0);
+
+	}
+
+}
---
+++ Programas del servicio/1° Bimestre/Enero/Java/workspace/proyect1/src/main/java/com/mycompany/proyect1/Proyect1.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyect1;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Efra
+ */
+public class Proyect1 {
+
+    public static void main(String[] args) {
+
+		Panel mipanel = new Panel();
+                mipanel.panelMostrar();
+		
+    }
+}
+
+class Panel{
+    public void panelMostrar(){
+        String primerNumero;
+		String segundoNumero;
+		
+		int numero1;
+		int numero2;
+		int suma;
+		
+		primerNumero = JOptionPane.showInputDialog("Escriba el primer entero" );
+		segundoNumero = JOptionPane.showInputDialog("Escriba el segundo entero" );
+		
+		numero1 = Integer.parseInt(primerNumero);
+		numero2 = Integer.parseInt(segundoNumero);
+		
+		suma = numero1 + numero2;
+		
+		JOptionPane.showMessageDialog(null, "La suma es " + suma, "Resultados", JOptionPane.PLAIN_MESSAGE);
+                System.exit(0);
+    }
+}
